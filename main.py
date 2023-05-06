05749cd8f95728cca54743ad86e27f5b226df4f4
pasldtv <122740489+pasldtv@users.noreply.github.com>
2023-05-06 10:39:37 +0500

    Обновить main.ts, main.py

--- main.py
+++ main.py
@@ -1,37 +1,43 @@
+speed_X = 0
+play = 0
+time = 0
+basket: game.LedSprite = None
+egg: game.LedSprite = None
+egg = game.create_sprite(randint(0, 4), 0)
+basket = game.create_sprite(2, 4)
+time = 300
+score = 0
+play = 1
+basic.pause(time)
+
+
+
 def on_button_pressed_a():
-    global ускорение_X
-    while игра == 1:
-        яйцо.change(LedSpriteProperty.Y, 1)
-        basic.pause(время)
-        ускорение_X = input.acceleration(Dimension.X)
-        корзина.set(LedSpriteProperty.X,
-            2 + min(2, max(-2, Math.round(ускорение_X / 200))))
-        if яйцо.get(LedSpriteProperty.Y) == 4:
-            if корзина.is_touching(яйцо):
+    global speed_X
+    while play == 1:
+        egg.change(LedSpriteProperty.Y, 1)
+        basic.pause(time)
+        speed_X = input.acceleration(Dimension.X)
+        basket.set(LedSpriteProperty.X,
+            2 + min(2, max(-2, Math.round(speed_X / 200))))
+        if egg.get(LedSpriteProperty.Y) == 4:
+            if basket.is_touching(egg):
                 game.add_score(1)
             else:
                 basic.show_icon(IconNames.NO)
-            яйцо.set(LedSpriteProperty.Y, 0)
-            яйцо.set(LedSpriteProperty.X, randint(0, 4))
-            basic.pause(время)
+            egg.set(LedSpriteProperty.Y, 0)
+            egg.set(LedSpriteProperty.X, randint(0, 4))
+            basic.pause(time)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+
+
 def on_button_pressed_ab():
-    global игра
-    игра = 0
+    global play
+    play = 0
     basic.show_number(game.score())
     basic.pause(500)
     game.game_over()
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
-ускорение_X = 0
-игра = 0
-время = 0
-корзина: game.LedSprite = None
-яйцо: game.LedSprite = None
-яйцо = game.create_sprite(randint(0, 4), 0)
-корзина = game.create_sprite(2, 4)
-время = 300
-очки = 0
-игра = 1
-basic.pause(время)
+
--- main.ts
+++ main.ts
@@ -1,39 +1,39 @@
+let speed_X = 0
+let play = 0
+let time = 0
+let basket : game.LedSprite = null
+let egg : game.LedSprite = null
+egg = game.createSprite(randint(0, 4), 0)
+basket = game.createSprite(2, 4)
+time = 300
+let score = 0
+play = 1
+basic.pause(time)
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
-    while (игра == 1) {
-        яйцо.change(LedSpriteProperty.Y, 1)
-        basic.pause(время)
-        ускорение_X = input.acceleration(Dimension.X)
-        корзина.set(LedSpriteProperty.X, 2 + Math.min(2, Math.max(-2, Math.round(ускорение_X / 200))))
-        if (яйцо.get(LedSpriteProperty.Y) == 4) {
-            if (корзина.isTouching(яйцо)) {
+    while (play == 1) {
+        egg.change(LedSpriteProperty.Y, 1)
+        basic.pause(time)
+        speed_X = input.acceleration(Dimension.X)
+        basket.set(LedSpriteProperty.X, 2 + Math.min(2, Math.max(-2, Math.round(speed_X / 200))))
+        if (egg.get(LedSpriteProperty.Y) == 4) {
+            if (basket.isTouching(egg)) {
                 game.addScore(1)
             } else {
                 basic.showIcon(IconNames.No)
             }
             
-            яйцо.set(LedSpriteProperty.Y, 0)
-            яйцо.set(LedSpriteProperty.X, randint(0, 4))
-            basic.pause(время)
+            egg.set(LedSpriteProperty.Y, 0)
+            egg.set(LedSpriteProperty.X, randint(0, 4))
+            basic.pause(time)
         }
         
     }
 })
 input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
     
-    игра = 0
+    play = 0
     basic.showNumber(game.score())
     basic.pause(500)
     game.gameOver()
 })
-let ускорение_X = 0
-let игра = 0
-let время = 0
-let корзина : game.LedSprite = null
-let яйцо : game.LedSprite = null
-яйцо = game.createSprite(randint(0, 4), 0)
-корзина = game.createSprite(2, 4)
-время = 300
-let очки = 0
-игра = 1
-basic.pause(время)
